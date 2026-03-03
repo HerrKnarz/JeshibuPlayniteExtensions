0428a2d10097bcdd997232fe9cf8ea298b3f4d7f
Alexander Heidn <40741234+HerrKnarz@users.noreply.github.com>
2026-03-03 11:07:34 +0100

    Removed braces from one liners and changed members to variables.

--- source/LaunchBoxMetadata/ScreenshotUtilitiesIntegration/ScreenshotUtilitiesIntegrator.cs
+++ source/LaunchBoxMetadata/ScreenshotUtilitiesIntegration/ScreenshotUtilitiesIntegrator.cs
@@ -15,36 +15,27 @@ namespace LaunchBoxMetadata.ScreenshotUtilitiesIntegration;
 
 public class ScreenshotUtilitiesIntegrator(LaunchBoxMetadata plugin, LaunchBoxMetadataSettings settings, LaunchBoxWebScraper scraper, IPlatformUtility platformUtility)
 {
-    private Game _game;
-    private ScreenshotGroup _screenshotGroup;
-
-    private string GetDatabaseUrl(string databaseId = default)
+    private string GetDatabaseUrl(Game game, ScreenshotGroup screenshotGroup, string databaseId = default)
     {
         string searchUrl = default;
 
         if (databaseId != default && long.TryParse(databaseId, out var dbIdLong))
-        {
             searchUrl = scraper.GetLaunchBoxGamesDatabaseUrl(dbIdLong);
-        }
+
+        if (searchUrl == default)
+            searchUrl = screenshotGroup.GameIdentifier;
 
         if (searchUrl == default)
         {
-            searchUrl = _screenshotGroup.GameIdentifier;
-        }
-
-        if (searchUrl == default)
-        {
-            var link = MetadataHelper.GetLink(_game, new System.Text.RegularExpressions.Regex(@"gamesdb\.launchbox-app\.com\/games\/details\/"));
+            var link = MetadataHelper.GetLink(game, new System.Text.RegularExpressions.Regex(@"gamesdb\.launchbox-app\.com\/games\/details\/"));
 
             if (link != null)
-            {
                 searchUrl = link.Url;
-            }
         }
 
         if (searchUrl == default)
         {
-            var foundGame = LaunchBoxHelper.FindGameInBackground(new LaunchBoxDatabase(plugin.GetPluginUserDataPath()), _game, platformUtility);
+            var foundGame = LaunchBoxHelper.FindGameInBackground(new LaunchBoxDatabase(plugin.GetPluginUserDataPath()), game, platformUtility);
 
             searchUrl = scraper.GetLaunchBoxGamesDatabaseUrl(foundGame.DatabaseID);
         }
@@ -52,9 +43,9 @@ public class ScreenshotUtilitiesIntegrator(LaunchBoxMetadata plugin, LaunchBoxMe
         return searchUrl;
     }
 
-    public async Task<bool> LoadScreenshotsFromSourceAsync()
+    public async Task<bool> LoadScreenshotsFromSourceAsync(Game game, ScreenshotGroup screenshotGroup)
     {
-        var url = _screenshotGroup.GameIdentifier;
+        var url = screenshotGroup.GameIdentifier;
 
         var updated = false;
 
@@ -62,24 +53,22 @@ public class ScreenshotUtilitiesIntegrator(LaunchBoxMetadata plugin, LaunchBoxMe
         {
             // TODO: change settings.Background to dedicated settings for screenshots!
             var whitelistedImgTypes = settings.Background.ImageTypes.Where(t => t.Checked).Select(t => t.Name).ToList();
-            var whitelistedRegions = LaunchBoxHelper.GetWhitelistedRegions(_game?.Regions, settings);
+            var whitelistedRegions = LaunchBoxHelper.GetWhitelistedRegions(game?.Regions, settings);
 
             var imageDetails = LaunchBoxHelper.GetImageDetails(scraper, url).Where(i => LaunchBoxHelper.FilterImage(i, whitelistedImgTypes, whitelistedRegions, settings.Background)).ToList();
 
             if (imageDetails == null || !imageDetails.Any())
-            {
                 return false;
-            }
 
             var filteredimageDetails = imageDetails
-                .Where(i => !_screenshotGroup.Screenshots.Any(es => es.Path.Equals(i.Url)))
+                .Where(i => !screenshotGroup.Screenshots.Any(es => es.Path.Equals(i.Url)))
                 .OrderBy(i => whitelistedImgTypes.IndexOf(i.Type))
                 .ThenBy(i => whitelistedRegions.IndexOf(i.Region))
                 .ToList();
 
             foreach (var image in filteredimageDetails)
             {
-                _screenshotGroup.Screenshots.Add(new Screenshot(image.Url)
+                screenshotGroup.Screenshots.Add(new Screenshot(image.Url)
                 {
                     ThumbnailPath = image.ThumbnailUrl,
                     Name = image.Type,
@@ -103,51 +92,39 @@ public class ScreenshotUtilitiesIntegrator(LaunchBoxMetadata plugin, LaunchBoxMe
         {
             // return when the main addon isn't installed.
             if (!ScreenshotHelper.IsScreenshotUtilitiesInstalled || game == null)
-            {
                 return false;
-            }
 
-            // set game and load the file.
-            _game = game;
-
-            var fileExists = false;
-
-            (fileExists, _screenshotGroup) = ScreenshotHelper.LoadGroup(_game, plugin.ProviderName, plugin.Id);
+            // load the file.
+            (var fileExists, var screenshotGroup) = ScreenshotHelper.LoadGroup(game, plugin.ProviderName, plugin.Id);
 
             // return if we don't want to force an update and the last update was inside the days configured.
             if (!forceUpdate
-                && _screenshotGroup.LastUpdate != null
-                && (_screenshotGroup.LastUpdate > DateTime.Now.AddDays(daysSinceLastUpdate * -1)))
-            {
+                && screenshotGroup.LastUpdate != null
+                && (screenshotGroup.LastUpdate > DateTime.Now.AddDays(daysSinceLastUpdate * -1)))
                 return false;
-            }
 
             // Get the identifying url to search for.
-            var searchUrl = GetDatabaseUrl(databaseId);
+            var searchUrl = GetDatabaseUrl(game, screenshotGroup, databaseId);
 
             if (string.IsNullOrEmpty(searchUrl))
-            {
                 return false;
-            }
 
             // Return if a game was searched and it's the one we already have.
-            if (searchUrl != default && searchUrl.Equals(_screenshotGroup.GameIdentifier))
-            {
+            if (searchUrl != default && searchUrl.Equals(screenshotGroup.GameIdentifier))
                 return false;
-            }
 
             // We need to reset the file if we got a new id from the method call and it's not the
             // same we already got.
-            if (!fileExists || (databaseId != default && !searchUrl.Equals(_screenshotGroup.GameIdentifier)))
+            if (!fileExists || (databaseId != default && !searchUrl.Equals(screenshotGroup.GameIdentifier)))
             {
-                _screenshotGroup.GameIdentifier = searchUrl;
+                screenshotGroup.GameIdentifier = searchUrl;
 
-                _screenshotGroup.Screenshots.Clear();
+                screenshotGroup.Screenshots.Clear();
             }
 
-            var updated = await LoadScreenshotsFromSourceAsync();
+            var updated = await LoadScreenshotsFromSourceAsync(game, screenshotGroup);
 
-            ScreenshotHelper.SaveScreenshotGroupJson(game, _screenshotGroup);
+            ScreenshotHelper.SaveScreenshotGroupJson(game, screenshotGroup);
 
             return updated;
         }
@@ -165,9 +142,7 @@ public class ScreenshotUtilitiesIntegrator(LaunchBoxMetadata plugin, LaunchBoxMe
             var results = new LaunchBoxDatabase(plugin.GetPluginUserDataPath()).SearchGames(searchTerm).Select(LaunchBoxGameItemOption.FromLaunchBoxGame).ToList();
 
             if (!results?.Any() ?? true)
-            {
                 return null;
-            }
 
             var result = new List<ScreenshotSearchResult>(results.Select(item => new ScreenshotSearchResult
             {
